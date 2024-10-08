f8df6a480b219ea183148b1470f6a51b0c60ba6a
zxeloy <139012590+Eloy527@users.noreply.github.com>
2024-10-08 22:46:00 +0300

    add login and made acount and changed styles

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # How Start Sait
 # .venv\Scripts\activate
-# pip install django
 # cd chatup 
 # python manage.py runserver   
--- chatup/registration/views.py
+++ chatup/registration/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import SignUpForm
 
-
-def sign_in(request):
-    return render(request, 'registration/html/sign_in.html')
+class SignUp(generic.CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
